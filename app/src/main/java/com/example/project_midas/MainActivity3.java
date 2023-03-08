d1105b2a6bc3ddd08433f36a1149ccf8ed97e873
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2023-03-08 21:18:10 +0100

    project_midas 1,1,15 connectie met MainActivity3 en MainActivity4

--- app/src/main/java/com/example/project_midas/MainActivity3.java
+++ app/src/main/java/com/example/project_midas/MainActivity3.java
@@ -1,22 +1,43 @@
 package com.example.project_midas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    //constraints layout aanmaken
+    private ConstraintLayout constraintLayout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
+        //view functies
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
         getWindow().setNavigationBarColor(getResources().getColor(R.color.black));
         getWindow().setStatusBarColor(getResources().getColor(R.color.black));
+
+        //constraints linken
+        constraintLayout1 = findViewById(R.id.constraint_2);
+
+        //functions
+        clickFun_1();
+    }
+
+    public void clickFun_1(){
+        constraintLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity4.class);
+                startActivity(intent);
+            }
+        });
     }
 }
